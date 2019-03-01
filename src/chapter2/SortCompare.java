59b64903543ed9d548b502b3faa49c1ccfc771a5
fengxiaofan <you@example.com>
2019-03-01 17:34:32 +0800

    3-1

--- src/chapter2/SortCompare.java
+++ src/chapter2/SortCompare.java
@@ -1,16 +1,26 @@
 package chapter2;
 
+
+import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.StdRandom;
 
 public class SortCompare {
 	public static double time(String alg, Comparable[] a) {
+		if (alg.equals("Insertion")) InsertionSort.sort(a);
+		if (alg.equals("Selection")) SelectionSort.sort(a);
+		if (alg.equals("Shell")) Shell.sort(a);
+		//if (alg.equals("Merge")) Merge.sort(a);
+		//if (alg.equals("Quick")) Quick.sort(a);
+		//if (alg.equals("Heap")) Heap.sort(a);
 		return 2.0;
 	}
 	
 	public static double timeRandomInput(String alg, int N, int T) {
-		double total = 0;
+		// 将T个长度为N的数组排序
+		double total = 0.0;
 		Double[] a = new Double[N];
 		for (int t = 0; t < T; t++) {
+			//进行一次测试，生成一个数组并排序
 			for (int i = 0; i < N; i++) {
 				a[i] = StdRandom.uniform();
 			}
@@ -26,6 +36,7 @@ public class SortCompare {
 		int T = Integer.parseInt(args[3]);
 		double t1 = timeRandomInput(alg1, N, T);
 		double t2 = timeRandomInput(alg2, N, T);
-		
+		StdOut.printf("For %d random Doubles\n  %s is", N,alg1);
+		StdOut.printf(" %.1f times faster than %s\n", t2/t1, alg2);
 	}
 }
